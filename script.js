dc104fac04017e367ab716a4fcea3a98729410b7
Paiktio <151546979+Fire-Frog-Fuel@users.noreply.github.com>
2025-05-30 09:00:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function loadCSV(file, tableId, headId, bodyId) {
         .then(response => response.text())
         .then(data => {
             const parsed = Papa.parse(data, { skipEmptyLines: true });
-
+            const rows = parsed.data;
             const [headers, ...entries] = rows;
 
             document.getElementById(headId).innerHTML = '';
